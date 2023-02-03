91f9df94cfdb74bc03db2a01f2100904265723bf
chiamaka28 <maduikechiamaka4@gmail.com>
2023-02-03 23:51:55 +0100

    implemented giving the boxes colors

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const colorDisplay = document.querySelector(".color");
+const options = document.querySelectorAll(".options")
 
 //for generating the random color
 const color = `${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`;
 console.log(color)
 
-
-
 colorDisplay.innerHTML = color;
+
+const optionsColors = [`${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`,
+`${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`,
+`${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`,
+`${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`,
+`${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)},${Math. floor(Math. random() * 256)}`,
+ color];
+console.log(optionsColors)
+
+
+
+
+for (let i = 0; i < options.length; i++) {
+   options[i].style.background = `rgb(${optionsColors[i]})`;   
+}
+
